46d8ba01951ba6299581537bc363a11c76187e26
Erica Zimmerman <ericanzimmerman@gmail.com>
2023-07-10 15:45:59 -0500

    options to spots

--- backend/db/migrations/20230709171033-create-spot.js
+++ backend/db/migrations/20230709171033-create-spot.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Spots', {
+    await queryInterface.createTable(options, {
       id: {
         allowNull: false,
         autoIncrement: true,
