a71f01ca1cf11b821cc55a96c4bc49591ed9bb4e
Erica Zimmerman <ericanzimmerman@gmail.com>
2023-07-10 14:01:25 -0500

    added hooks

--- backend/db/migrations/20230710184930-add-userId-to-booking.js
+++ backend/db/migrations/20230710184930-add-userId-to-booking.js
@@ -1,6 +1,8 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -14,7 +16,9 @@ module.exports = {
       references: {
         model: 'Users',
         key: 'id'
-      }
+      },
+      onDelete: 'CASCADE',
+      hooks: true
     })
   },
 
