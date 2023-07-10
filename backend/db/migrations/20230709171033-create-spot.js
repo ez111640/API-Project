1ecf543ad5b089fd9db5fdb1339b39d436560395
Erica Zimmerman <ericanzimmerman@gmail.com>
2023-07-10 15:39:14 -0500

    options to up

--- backend/db/migrations/20230709171033-create-spot.js
+++ backend/db/migrations/20230709171033-create-spot.js
@@ -1,5 +1,11 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Spots', {
@@ -52,9 +58,9 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    await queryInterface.dropTable(options);
   }
 };
